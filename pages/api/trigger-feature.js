5a9851664208764dc67a0d73cde91eda622ffe37
Hans Ott <hansott@hotmail.be>
2023-05-22 16:41:20 +0200

    🐛 Use callback to wait until event is sent to Segment
    In serverless environments, it's a bit tricky

--- pages/api/trigger-feature.js
+++ pages/api/trigger-feature.js
@@ -5,12 +5,17 @@ async function handler(request, response) {
   const { featureName, accountId } = request.body;
   const user = request.session.get("user");
 
-  analytics.track({
-    userId: user.id,
-    event: featureName,
-    context: {
-      groupId: accountId,
-    },
+  await new Promise((resolve) => {
+    analytics.track(
+      {
+        userId: user.id,
+        event: featureName,
+        context: {
+          groupId: accountId,
+        },
+      },
+      resolve
+    );
   });
 
   return response.send();
--- util/analytics.js
+++ util/analytics.js
@@ -4,3 +4,5 @@ export const analytics = new Analytics({
   writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
   maxEventsInBatch: 1,
 });
+
+analytics.on("error", (error) => console.log(error));
